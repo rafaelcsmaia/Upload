2a451ac6a708c1d43f1e325fe8492f5c7cd5a23b
RAFAEL MAIA <rafael.c.maia@ericsson.com>
2018-07-19 15:24:44 -0300

    Tweaked SQL for auto alarm

--- Extratistico/Models/Repositorios/InfoSyncRepository.cs
+++ Extratistico/Models/Repositorios/InfoSyncRepository.cs
@@ -13,7 +13,7 @@ namespace Extratistico.Models.Repositorios
         {
             List<InfoSync> resp = new List<InfoSync>();
             MySqlConnection cnn = new MySqlConnection("server=6dca2b28-04bc-4f05-99e9-a4c200c82af7.mysql.sequelizer.com;database=db6dca2b2804bc4f0599e9a4c200c82af7;uid=glkypyllkirkaavi;pwd=Pc7BqxcHQtitRP4ikFxMdGWcvHWFvnNgyeCb5YFZzXprXhTSxGnCLmphn4gQJCnB");
-            MySqlCommand cmd = new MySqlCommand(string.Format("SELECT *from INFOSYNC where date_format(data,'%d/%m/%Y') = date_format(CURRENT_TIMESTAMP(),'%d/%m/%Y')"), cnn);
+            MySqlCommand cmd = new MySqlCommand(string.Format("SELECT a.message, a.status, a.data, a.id from INFOSYNC a inner join (SELECT max(status) as status, id from INFOSYNC group by id) b on a.id=b.id and a.status=b.status where date_format(data,'%d/%m/%Y') = date_format(CURRENT_TIMESTAMP(),'%d/%m/%Y')"), cnn);
             cnn.Open();
             MySqlDataReader r = cmd.ExecuteReader();
             while (r.Read())
