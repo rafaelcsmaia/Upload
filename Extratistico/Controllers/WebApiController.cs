73190cea20369672663c2a1277c048f22b46fcbb
rafaelcsmaia <rafaelcsmaia@gmail.com>
2018-10-04 15:45:07 -0300

    APK

--- Extratistico/Controllers/WebApiController.cs
+++ Extratistico/Controllers/WebApiController.cs
@@ -12,6 +12,7 @@ using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -61,6 +62,32 @@ namespace Extratistico.Controllers
                 return new { Result = false };
             }
                         
+        }
+
+        [HttpGet]
+        public object AutoPushAPK()
+        {
+
+                byte[] _Buffer = null;
+
+
+                // Open file for reading
+                System.IO.FileStream _FileStream = new System.IO.FileStream(System.Web.HttpContext.Current.Request.MapPath("~\\App_Data\\AutoPush\\AutoPush.apk"), System.IO.FileMode.Open, System.IO.FileAccess.Read);
+
+                // attach filestream to binary reader
+                System.IO.BinaryReader _BinaryReader = new System.IO.BinaryReader(_FileStream);
+
+                // get total byte length of the file
+                long _TotalBytes = new System.IO.FileInfo(System.Web.HttpContext.Current.Request.MapPath("~\\App_Data\\AutoPush\\AutoPush.apk")).Length;
+
+                // read entire file into buffer
+                _Buffer = _BinaryReader.ReadBytes((Int32)_TotalBytes);
+
+                // close file reader
+                _FileStream.Close();
+                _FileStream.Dispose();
+                _BinaryReader.Close();
+            return new { Result = _Buffer };
         }
 
 
